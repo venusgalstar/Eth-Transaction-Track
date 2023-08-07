34810a2a8f1960bdfb619f3f30cfaedfc61916ce
3589810 <3589810@gmail.com>
2023-08-07 15:23:30 +0200

    finished transfer

--- sync-pls.py
+++ sync-pls.py
@@ -51,9 +51,9 @@ def create_database():
             amountIn TEXT,
             amountOut TEXT,
             inTokenName TEXT,
-            inTokenNymbol TEXT,              
+            inTokenSymbol TEXT,              
             outTokenName TEXT,
-            outTokenNymbol TEXT,
+            outTokenSymbol TEXT,
             inTokenAddress TEXT,
             outTokenAddress TEXT,
             transactionHash TEXT
@@ -63,36 +63,14 @@ def create_database():
     conn.commit()
     conn.close()
 
-# Insert data into sqlite database
-
-
-def insert_data(data):
-    conn = sqlite3.connect('transactions.db')
-    c = conn.cursor()
-
-    c.execute('''
-        INSERT INTO transactions VALUES (
-            :time, :blockHash, :blockNumber, :fromAddress, :gas, :gasPrice,
-            :hash, :input, :nonce, :toAddress,
-            :transactionIndex, :value, :type, :contractAddress,
-            :cumulativeGasUsed, :effectiveGasPrice, :gasUsed, :status,
-            :transactionHash, :contractTo, :contractValue
-        )
-    ''', data)
-
-    conn.commit()
-    conn.close()
-
 # Insert transfer data into sqlite database
-
-
 def insert_transfer(data):
-    conn = sqlite3.connect('transactions.db')
+    conn = sqlite3.connect(dbName)
     c = conn.cursor()
 
     c.execute('''
         INSERT INTO transfer VALUES (
-            :blockNumber, :fromAddress, :toAddress, :amount, :name, :sybmol, :address,
+            :blockNumber, :fromAddress, :toAddress, :amount, :name, :symbol, :address,
             :transactionHash
         )
     ''', data)
@@ -123,64 +101,17 @@ def insert_swap(data):
 if not os.path.exists(dbName):
     create_database()
 
-# Adds all transactions from Ethereum block
-def insertTxsFromBlock(block):
-    blockid = block['number']
-    time = block['timestamp']
-    for txNumber in range(0, len(block.transactions)):
-        trans = block.transactions[txNumber]
-
-        print(trans)
-
-
-        # Perform receipt and strip operations lateron
-        transReceipt = web3.eth.getTransactionReceipt(trans['hash'])
-
-        data = {
-            'time': time,
-            'blockHash': trans['hash'].hex(),
-            'blockNumber': blockid,
-            'fromAddress': trans['from'],
-            'gas': trans['gas'],
-            'gasPrice': trans['gasPrice'],
-            'hash': trans['hash'].hex(),
-            'input': trans['input'],
-            'nonce': trans['nonce'],
-            'toAddress': trans['to'],
-            'transactionIndex': trans['transactionIndex'],
-            'value': str(trans['value']),
-            'type': trans['type'],
-            'contractAddress': transReceipt['contractAddress'],
-            'cumulativeGasUsed': transReceipt['cumulativeGasUsed'],
-            'effectiveGasPrice': transReceipt['effectiveGasPrice'],
-            'gasUsed': transReceipt['gasUsed'],
-            'status': bool(transReceipt['status']),
-            'transactionHash': transReceipt['transactionHash'].hex(),
-            'contractTo': '',
-            'contractValue': ''
-        }
-
-        # try:
-        insert_data(data)
-        # except Exception as e:
-        #     print(str(e))
-        #     print(data)
-        #     pass
-
 def handle_event(event):
     print(event['address'])
     contract = web3.eth.contract(address = event['address'], abi = TOKEN_ABI)
     name = contract.functions.name().call()
     symbol = contract.functions.symbol().call()
 
-    # :blockNumber, :fromAddress, :toAddress, :amount, :name, :sybmol, :address,
-    #         :transactionHash,
-
     data = {
         'blockNumber': event['blockNumber'],
         'fromAddress': event['topics'][1].hex().replace("0x000000000000000000000000","0x"),
         'toAddress': event['topics'][2].hex().replace("0x000000000000000000000000","0x"),
-        'amount': int(event['data'],16),
+        'amount': str(int(event['data'],16)),
         'name': name,
         'symbol': symbol,
         'address': event['address'],
@@ -190,11 +121,9 @@ def handle_event(event):
     insert_transfer(data)
 
 
-
 def log_loop(event_filter):
-    while True:
-        for event in event_filter.get_all_entries():
-            handle_event(event)
+    for event in event_filter.get_all_entries():
+        handle_event(event)
 
 
 # Fetch all of new (not in index) Ethereum blocks and add transactions to index
@@ -203,8 +132,9 @@ while True:
     conn = sqlite3.connect(dbName)
     c = conn.cursor()
     c.execute("SELECT MAX(blockNumber) FROM transfer")
-    max_block_id = 17330000 #c.fetchone()
-    # max_block_id = max_block_id[0]
+    max_block_id = c.fetchone()
+    max_block_id = max_block_id[0]
+    conn.close()
 
     if max_block_id is None:
         max_block_id = startBlock
@@ -213,7 +143,8 @@ while True:
 
     endblock = int(web3.eth.blockNumber) - int(confirmationBlocks)
     checkingBlock = max_block_id + 1 
-    event_signature_hash = web3.keccak(text="Transfer(address,address,uint256)").hex()
+    transfer_event_topic = web3.keccak(text="Transfer(address,address,uint256)").hex()
+    swap_event_topic = web3.keccak(text="Transfer(address,address,uint256)").hex()
 
     if checkingBlock > endblock:
         checkingBlock = endblock
@@ -222,7 +153,7 @@ while True:
         log_filter = web3.eth.filter({
             "fromBlock": max_block_id,
             "toBlock": checkingBlock,
-            "topics": [event_signature_hash]
+            "topics": [transfer_event_topic]
         })
         log_loop(log_filter)
 
--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import sqlite3
 
 dbName = "transactions.db"
 
-def get_transactions_from_address(address):
+def get_transactions_from_address():
     conn = sqlite3.connect(dbName)
     c = conn.cursor()
 
     # Use the SELECT statement to get all transactions from the specified address
-    c.execute("SELECT * FROM transactions WHERE fromAddress = ?", (address,))
+    c.execute("SELECT * FROM transfer")
 
     # Fetch all the rows - each row represents a transaction
     transactions = c.fetchall()
@@ -17,5 +17,5 @@ def get_transactions_from_address(address):
 
     return transactions
 
-print(get_transactions_from_address("0xc17b1e62eAEf2805F664ed44972FCc7E6647474A"))
+print(get_transactions_from_address())
 # 17,270,687
