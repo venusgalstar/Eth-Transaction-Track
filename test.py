44f0bb013f511c0bd210e50a01db90e23cd69df9
3589810 <3589810@gmail.com>
2023-08-07 18:24:21 +0200

    update test script

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import sqlite3
 
-dbName = "transactions.db"
+address = "0x7361A0E33F717BaF49cd946f5B748E6AA81cC6Fb"
+dbName = "transfer.db"
 
-def get_transactions_from_address():
+def get_transfer_from_address(address):
     conn = sqlite3.connect(dbName)
     c = conn.cursor()
 
     # Use the SELECT statement to get all transactions from the specified address
-    c.execute("SELECT * FROM transfer")
+    c.execute("SELECT * FROM transfer WHERE fromAddress = ?", (address,))
 
     # Fetch all the rows - each row represents a transaction
     transactions = c.fetchall()
@@ -17,5 +18,21 @@ def get_transactions_from_address():
 
     return transactions
 
-print(get_transactions_from_address())
+def get_swap_from_address(address):
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+
+    # Use the SELECT statement to get all transactions from the specified address
+    c.execute("SELECT * FROM swap WHERE fromAddress = ?", (address,))
+
+    # Fetch all the rows - each row represents a transaction
+    transactions = c.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    return transactions
+
+print(get_transfer_from_address(address))
+print(get_swap_from_address(address))
 # 17,270,687
