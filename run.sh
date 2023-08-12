8efe2abf39ca3796d98cb5f369b19455a3d30e85
3589810 <3589810@gmail.com>
2023-08-12 16:35:05 +0200

    update scripts to run sequently

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo python3 sync-swap.py &
-sudo python3 sync-transfer.py &
-sudo python3 sync-pls.py &
+sudo python3 sync-swap.py 
+sudo python3 sync-transfer.py 
+sudo python3 sync-pls.py 
 wait 
 sudo python3 combine.py
